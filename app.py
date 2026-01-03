ee57eb01879d98b34cd1338223681499793dddb8
MeetPambhar <140879916+MeetPambhar@users.noreply.github.com>
2026-01-03 22:19:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = [
+        float(request.form['age']),
+        float(request.form['height']),
+        float(request.form['weight']),
+        float(request.form['bmi']),
+        float(request.form['ap_hi']),
+        float(request.form['ap_lo']),
+        int(request.form['cholesterol']),
+        int(request.form['gluc']),
+        int(request.form['smoke']),
+        int(request.form['alco']),
+        int(request.form['active'])
+    ]
+
+    prediction = model.predict([data])[0]
+    result = "Disease Detected" if prediction == 1 else "No Disease"
+
+    return render_template("index.html", prediction=result)
+
+if __name__ == "__main__":
+    app.run()
